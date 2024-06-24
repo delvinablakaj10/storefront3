78b3970a6eb1f9d8bbf58810f24b75a785fbb3eb
delvinablakaj10 <d.blakaj1108@gmail.com>
2024-06-24 13:48:12 +0200

    Smtp applied

---
+++ playground/views.py
@@ -0,0 +1,20 @@
+from django.core.mail import (BadHeaderError, EmailMessage, mail_admins,
+                              send_mail)
+from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
+
+
+def say_hello(request):
+    try:
+        # send_mail('subject', 'message', 'd.blakaj1108@gmail.com', ['d.blakaj1108@gmail.com'])
+        #mail_admins('subject', 'message', html_message='message')
+        # message = EmailMessage('subject', 'message', 'from@moshbuy.com',['d.blakaj1108@gmail.com'])
+        # message.attach_file('playground/static/images/dog.png')
+        # message.send()
+        message = BaseEmailMessage(template_name='emails/hello.html', context={'name': 'Test'})
+        message.send(['test@gmail.com'])
+    except BadHeaderError:
+        pass
+
+    return render(request, 'hello.html', {'name': 'Mosh'})
+
